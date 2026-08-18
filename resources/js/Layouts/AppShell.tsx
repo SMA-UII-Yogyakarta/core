600bda9c18286e46c7e6b21a42095de5a304f76a
Sandiko <33443311+sandikodev@users.noreply.github.com>
2026-08-18 14:32:05 +0700

    fix(layout): fix tablet icon sidebar position so it remains fixed when page scrolls (#47)

--- resources/js/Layouts/AppShell.tsx
+++ resources/js/Layouts/AppShell.tsx
@@ -117,7 +117,7 @@ export default function AppShell({ title, children }: AppShellProps) {
         <>
             {title && <Head title={title} />}
 
-            <div className="h-full flex flex-col bg-primary overflow-hidden">
+            <div className="h-screen max-h-screen flex flex-col bg-primary overflow-hidden">
                 <Toast />
 
                 {/* Mobile Header (lg:hidden) */}
